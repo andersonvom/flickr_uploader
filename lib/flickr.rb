2297b2ec737f827b7ae96fa64e3af59adbf841b9
Anderson Mesquita <andersonvom@gmail.com>
2014-03-14 15:02:04 -0700

    Configure basic Flickr object
    This configures FlickRaw with API key and secret and instantiates a
    new flickr object.

--- lib/flickr.rb
+++ lib/flickr.rb
@@ -3,6 +3,22 @@ require 'flickraw'
 
 
 class Flickr
+  attr_accessor :flickr
+
+  def initialize
+    config_app
+    $log.debug "Flickr account set up"
+  end
+
+  def config_app
+    $log.debug "Configuring app"
+    $log.debug "Config: API_KEY: #{Conf::API_KEY}"
+    $log.debug "Config: SHARED_SECRET: #{Conf::SHARED_SECRET}"
+    FlickRaw.api_key = Conf::API_KEY
+    FlickRaw.shared_secret = Conf::SHARED_SECRET
+    self.flickr = FlickRaw::Flickr.new
+  end
+
   def upload(file, set_name)
     $log.debug "Uploading #{file} to set #{set_name}"
   end
