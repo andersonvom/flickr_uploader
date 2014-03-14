d187fbeebe7c7c32887d0b288c066e1793344d67
Anderson Mesquita <andersonvom@gmail.com>
2014-03-14 14:16:59 -0700

    Get set name from a file and a root directory
    Files#set_name generates a set name for a given file based on the
    directory structure of the file, ignoring everything up to the
    root_dir.
    
    e.g.
        file = /path/to/root/subpath/leading/to/folder/file.jpg
        root_dir = /path/to/root
        will return the following set name:
        subpath-leading-to-folder
    
    If the file is in the root_dir itself, then its name will be used
    instead.
    
    e.g.
        file = /path/to/root/file.jpg
        root_dir = /path/to/root
        will return the following set name:
        root

--- lib/files.rb
+++ lib/files.rb
@@ -1,5 +1,14 @@
 module Files
 
+  def Files.set_name(file, root_dir)
+    current_dir = File.dirname(file)
+    set_name = current_dir.gsub(/#{root_dir}[\/]?/, '').gsub('/', '-')
+    if set_name.empty?
+      set_name = File.basename current_dir
+    end
+    set_name
+  end
+
   def Files.set_walk(*paths)
     paths.each do |path|
       $log.debug "Yielding path"
