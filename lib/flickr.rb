7670bc52919ba4428b027339486c690ccfcfc99b
Anderson Mesquita <andersonvom@gmail.com>
2014-03-14 15:07:21 -0700

    Add Flickr authentication
    Add basic authentication against Flickr and save the necessary token and
    secret for future use.

--- lib/flickr.rb
+++ lib/flickr.rb
@@ -3,10 +3,12 @@ require 'flickraw'
 
 
 class Flickr
-  attr_accessor :flickr
+  attr_accessor :flickr, :token, :secret
 
   def initialize
     config_app
+
+    authenticate
     $log.debug "Flickr account set up"
   end
 
@@ -17,6 +19,27 @@ class Flickr
     FlickRaw.api_key = Conf::API_KEY
     FlickRaw.shared_secret = Conf::SHARED_SECRET
     self.flickr = FlickRaw::Flickr.new
+  end
+
+  def authenticate
+    $log.debug "Authenticating"
+    info = flickr.get_request_token
+    request_token = info['oauth_token']
+    request_secret = info['oauth_token_secret']
+    verify_auth request_token, request_secret
+  end
+
+  def verify_auth(request_token, request_secret, perms = 'write')
+    $log.debug "Get URL for verification token"
+    auth_url = flickr.get_authorize_url(request_token, :perms => perms)
+    puts "Visit this URL to complete authentication: #{auth_url}"
+    puts "When you're done, paste the verification code below."
+    print "Verification code: "
+
+    verification_code = gets.strip
+    flickr.get_access_token(request_token, request_secret, verification_code)
+    self.token = flickr.access_token
+    self.secret = flickr.access_secret
   end
 
   def upload(file, set_name)
