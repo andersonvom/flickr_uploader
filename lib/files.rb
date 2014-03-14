642dfcb27d34d07811267de86060269d788860c8
Anderson Mesquita <andersonvom@gmail.com>
2014-03-14 14:29:37 -0700

    Implement Files#set_walk
    Files#set_walk will go through all the files in a given directory
    and yield both the file path and the set name to a Flickr object to
    upload them.

--- lib/files.rb
+++ lib/files.rb
@@ -11,8 +11,18 @@ module Files
 
   def Files.set_walk(*paths)
     paths.each do |path|
-      $log.debug "Yielding path"
-      yield path, path
+      root_dir = File.absolute_path(path)
+      $log.debug "Walking #{root_dir}"
+
+      matcher = File.join(root_dir, '', '**', '*')
+      files = Dir.glob(matcher)
+      files.each do |file|
+        next if File.directory? file
+
+        set_name = Files.set_name(file, root_dir)
+        $log.debug "Yield - Set: #{set_name} | File: #{file}"
+        yield file, set_name
+      end
     end
   end
 
