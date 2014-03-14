0b8b89f628534755c80a437d27a80a7050f9b68c
Anderson Mesquita <andersonvom@gmail.com>
2014-03-14 11:48:31 -0700

    Add Conf module
    Add basic config directory and API Key options to Conf module for easier
    access throughout the code.

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,17 @@ require 'logger'
 
 $log = Logger.new(STDOUT)
 $log.level = Logger::DEBUG
+
+
+module Conf
+  API_KEY="------------api_key-------------"
+  SHARED_SECRET="-----secret-----"
+
+  CONFIG_DIR = File.dirname(__FILE__)
+  ROOT_DIR = File.absolute_path(File.join(CONFIG_DIR, '..'))
+  LIB_DIR = File.join(ROOT_DIR, 'lib')
+  LOG_DIR = File.join(ROOT_DIR, 'log')
+
+  AUTH_FILE = File.join(CONFIG_DIR, 'flickr_auth.yaml')
+  LOG_FILE = File.join(LOG_DIR, 'flickr-uploadr.log')
+end
