dcf795f20b7742271f6dfcaed1485bb3e108ade1
Anderson Mesquita <andersonvom@gmail.com>
2014-04-18 13:35:09 -0500

    wip - read or create api file

--- lib/flickr_uploader/config.rb
+++ lib/flickr_uploader/config.rb
@@ -1,13 +1,41 @@
+require 'yaml'
+
+
 module FlickrUploader
   module Conf
     API_KEY="------------api_key-------------"
     SHARED_SECRET="-----secret-----"
 
-    CONFIG_DIR = File.dirname(__FILE__)
-    ROOT_DIR = File.absolute_path(File.join(CONFIG_DIR, '..'))
-    LIB_DIR = File.join(ROOT_DIR, 'lib')
-    LOG_DIR = File.join(ROOT_DIR, 'log')
+    DOT_DIR = File.absolute_path(File.join('~', '.flickr_uploader'))
+    API_FILE = File.join(DOT_DIR, 'flickr_api.yaml')
+    AUTH_FILE = File.join(DOT_DIR, 'flickr_auth.yaml')
+  end
 
-    AUTH_FILE = File.join(CONFIG_DIR, 'flickr_auth.yaml')
+  class Config
+    DOT_DIR = File.absolute_path(File.join('~', '.flickr_uploader'))
+    API_FILE = File.join(DOT_DIR, 'flickr_api.yaml')
+    AUTH_FILE = File.join(DOT_DIR, 'flickr_auth.yaml')
+
+    def self.flickr_api
+      LOG.error "init"
+      @@flickr_api ||= self.read_or_create_api_file
+    end
+
+    def self.read_or_create_api_file
+      flickr_api = nil
+      if File.exists? Config::API_FILE
+        flickr_api = YAML.load Config::API_FILE
+      else
+        print "Enter your API key: "
+        api_key = gets.chomp
+        print "Enter your API shared secret: "
+        secret = gets.chomp
+        flickr_api = { api_key: api_key, shared_secret: secret }
+        File.open Config::API_FILE do |file|
+          file.write flickr_api.to_yaml
+        end
+      end
+      flickr_api
+    end
   end
 end
