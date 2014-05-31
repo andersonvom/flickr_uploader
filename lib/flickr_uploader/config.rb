17a2da280493cd98a7cde4ffb4622290baed6ad8
Anderson Mesquita <andersonvom@gmail.com>
2014-05-31 17:17:10 -0400

    Fix DOT_DIR name
    Use ruby's standard Dir.home to get the current user's home directory
    for the app's config files.

--- lib/flickr_uploader/config.rb
+++ lib/flickr_uploader/config.rb
@@ -6,7 +6,7 @@ module FlickrUploader
     API_KEY="------------api_key-------------"
     SHARED_SECRET="-----secret-----"
 
-    DOT_DIR = File.absolute_path(File.join('~', '.flickr_uploader'))
+    DOT_DIR = File.join(Dir.home, '.flickr_uploader')
     API_FILE = File.join(DOT_DIR, 'flickr_api.yaml')
     AUTH_FILE = File.join(DOT_DIR, 'flickr_auth.yaml')
   end
