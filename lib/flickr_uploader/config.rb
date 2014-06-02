2ac49ac5ccc4ca40d372f557d6107660abcce296
Anderson Mesquita <andersonvom@gmail.com>
2014-06-02 19:59:22 -0400

    Improve readability of DOT_DIR constant
    Use ruby's native method to get the user's home directory to store
    misc information when running the app.

--- lib/flickr_uploader/config.rb
+++ lib/flickr_uploader/config.rb
@@ -12,7 +12,7 @@ module FlickrUploader
   end
 
   class Config
-    DOT_DIR = File.absolute_path(File.join('~', '.flickr_uploader'))
+    DOT_DIR = File.join(Dir.home, '.flickr_uploader')
     API_FILE = File.join(DOT_DIR, 'flickr_api.yaml')
     AUTH_FILE = File.join(DOT_DIR, 'flickr_auth.yaml')
 
