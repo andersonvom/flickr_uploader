feba3e8f6d41c70c3d6ad572658b1ce230d42a32
Anderson Mesquita <andersonvom@gmail.com>
2014-06-25 20:38:39 -0400

    Remove legacy Config code
    With the refactor of the ConfigFile (a much better approach), this is no
    longer needed.

--- lib/flickr_uploader/config.rb
+++ lib/flickr_uploader/config.rb
@@ -1,6 +1,3 @@
-require 'yaml'
-
-
 module FlickrUploader
   module Conf
     API_KEY="------------api_key-------------"
@@ -9,33 +6,5 @@ module FlickrUploader
     DOT_DIR = File.join(Dir.home, '.flickr_uploader')
     API_FILE = File.join(DOT_DIR, 'flickr_api.yaml')
     AUTH_FILE = File.join(DOT_DIR, 'flickr_auth.yaml')
-  end
-
-  class Config
-    DOT_DIR = File.join(Dir.home, '.flickr_uploader')
-    API_FILE = File.join(DOT_DIR, 'flickr_api.yaml')
-    AUTH_FILE = File.join(DOT_DIR, 'flickr_auth.yaml')
-
-    def self.flickr_api
-      LOG.error "init"
-      @@flickr_api ||= self.read_or_create_api_file
-    end
-
-    def self.read_or_create_api_file
-      flickr_api = nil
-      if File.exists? Config::API_FILE
-        flickr_api = YAML.load Config::API_FILE
-      else
-        print "Enter your API key: "
-        api_key = STDIN.gets.chomp
-        print "Enter your API shared secret: "
-        secret = STDIN.gets.chomp
-        flickr_api = { api_key: api_key, shared_secret: secret }
-        File.open Config::API_FILE do |file|
-          file.write flickr_api.to_yaml
-        end
-      end
-      flickr_api
-    end
   end
 end
